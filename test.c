8d22ab7f8e73c1f6d6ccdc62524de967e7b319ea
Julie Blaye <jblaye@paul-f3Ar3s2.clusters.42paris.fr>
2024-02-13 13:05:15 +0100

    added recent changes

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: jblaye <jblaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 10:32:54 by julieblaye        #+#    #+#             */
-/*   Updated: 2024/02/12 15:58:59 by jblaye           ###   ########.fr       */
+/*   Updated: 2024/02/13 12:56:12 by jblaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int	main(int ac, char **av)
 	init_tmap(&map);
 	map_parsing(av[1], &map);
 	ft_printstrtab(map.map_data);
+	ft_printinttab(map.player, 2);
+	ft_printinttab(map.exit, 2);
 	ft_fsplit(map.map_data);
 	return (0);
 }
