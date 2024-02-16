cc365cc343eea732d708219bb17967c15050ce99
Julie Blaye <jblaye@paul-f4Br7s1.clusters.42paris.fr>
2024-02-16 20:25:48 +0100

    added working window management - leaks to be managed. added ft hook to get keyboard moves. added loop hook to animate player

---
+++ srcs/ft_hook.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_hook.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jblaye <jblaye@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/15 17:45:12 by jblaye            #+#    #+#             */
+/*   Updated: 2024/02/16 20:16:49 by jblaye           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/so_long.h"
+
+int	ft_hook(int	keycode, t_data *data)
+{
+	//if (keycode == EXIT)
+	printf("x = %d\ny = %d\n\n", data->map.player[X], data->map.player[Y]);
+	if (keycode == UP)
+		return (player_up(data));
+	if (keycode == DOWN)
+		return (player_down(data));
+	// map->player[X]++;
+	// map->map_data[map->player[Y]][map->player[X] - 1] = SPACE;
+	// push_tile()
+	// map->map_data[map->player[Y]][map->player[X]] = PLAYER;
+	// printf("ca bouge\n");
+	return (1);
+}
--- srcs/window_management.c
+++ srcs/window_management.c
@@ -6,7 +6,7 @@
 /*   By: jblaye <jblaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 13:44:39 by jblaye            #+#    #+#             */
-/*   Updated: 2024/02/15 17:58:57 by jblaye           ###   ########.fr       */
+/*   Updated: 2024/02/16 18:44:53 by jblaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,26 +29,30 @@ t_mlx_data	create_window(int x, int y)
 
 
 
-void	free_all(void *mlx, void *mlx_win, void **assets)
+int	free_all(t_data *data)
 {
 	int	i;
 
 	i = NB_ASSETS;
 	while (i > 0)
 	{
-		mlx_destroy_image(mlx, assets[i - 1]);
+		if (data->assets[i - 1] != 0)
+			mlx_destroy_image(data->mlx.mlx, data->assets[i - 1]);
 		i--;
 	}
-	mlx_destroy_display(mlx);
-	mlx_clear_window(mlx, mlx_win);
-	mlx_destroy_window(mlx, mlx_win);
-	free(mlx);
+	mlx_destroy_display(data->mlx.mlx);
+	//mlx_clear_window(mlx, mlx_win);
+	mlx_destroy_window(data->mlx.mlx, data->mlx.mlx_win);
+	free(data->mlx.mlx);
+	free(data->mlx.mlx_win);
+	return (0);
 }
 
-void	terminate_window(t_mlx_data mlx, t_map *map, void **assets)
+void	terminate_window(t_data *data)
 {
-	mlx_hook(mlx.mlx_win, 17, 0, mlx_loop_end, mlx.mlx);
-	mlx_key_hook(mlx.mlx_win, ft_hook, map);
-	mlx_loop(mlx.mlx);
-	free_all(mlx.mlx, mlx.mlx_win, assets);
+	mlx_loop_hook(data->mlx.mlx, &render, data);
+	mlx_hook(data->mlx.mlx_win, 17, 0, mlx_loop_end, data->mlx.mlx);
+	mlx_key_hook(data->mlx.mlx_win, ft_hook, data);
+	mlx_loop(data->mlx.mlx);
+	//free_all(data);
 }
