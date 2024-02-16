6479f91c3b9863255a3d9dfc446518d73b76af03
Julie Blaye <jblaye@paul-f4Br7s1.clusters.42paris.fr>
2024-02-16 20:28:50 +0100

    latest version of the test main

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: jblaye <jblaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 10:32:54 by julieblaye        #+#    #+#             */
-/*   Updated: 2024/02/15 17:57:40 by jblaye           ###   ########.fr       */
+/*   Updated: 2024/02/16 18:11:43 by jblaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,28 +16,23 @@
 
 int	main(int ac, char **av)
 {
-	t_map 		map;
-	t_mlx_data	mlx;
+	t_data		data;
 	(void) ac;
 	
-	init_tmap(&map);
-	if (map_parsing(av[1], &map) == 0)
+	init_tmap(&(data.map));
+	data.count = 0;
+	if (map_parsing(av[1], &(data.map)) == 0 || find_path(&(data.map)) == 0)
 		return (0);
-	printf("PATH = %d\n", find_path(&map));
-	mlx = create_window(map.size[X], map.size[Y]);
-	if (!mlx.mlx_win)
+	data.mlx = create_window(data.map.size[X], data.map.size[Y]);
+	if (!data.mlx.mlx_win)
 		return (ft_dprintf(2, "FAIL\n"), 0);
-	void **table = assets_table(mlx.mlx);
-	if (!table)
+	data.assets = assets_table(data.mlx.mlx);
+	if (!data.assets)
 		return (0);
 	//push_tile(mlx, T_BL, 0, 0);
-	if (render_border(mlx, map, table) == 0)
-		return (0);
-	if (render_fix_content(mlx, map, table) == 0)
-		return (0);
-	if (render_moving_content(mlx, map, table) == 0)
-		return (0);
-	terminate_window(mlx, &map, table);
-	ft_fsplit(map.map_data);
+	// if (render(&data) == 0)
+	// 	return (0);
+	terminate_window(&data);
+	ft_fsplit(data.map.map_data);
 	return (0);
 }
