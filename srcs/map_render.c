e3b7046d2b225103b1dae1f5b183fd15b4189c75
Julie Blaye <jblaye@paul-f4Br7s1.clusters.42paris.fr>
2024-02-16 20:26:41 +0100

    created all render functions that push the tiles to the img depending on the current map matrix

--- srcs/map_render.c
+++ srcs/map_render.c
@@ -5,120 +5,22 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jblaye <jblaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/14 15:18:11 by jblaye            #+#    #+#             */
-/*   Updated: 2024/02/15 17:01:48 by jblaye           ###   ########.fr       */
+/*   Created: 2024/02/16 13:18:05 by jblaye            #+#    #+#             */
+/*   Updated: 2024/02/16 19:41:12 by jblaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
-// t_img	generate_image(t_map map, t_mlx_data mlx)
-// {
-// 	t_img	img;
-	
-// 	img.img_ptr = mlx_new_image(mlx.mlx, map.size[X] * TILE, map.size[Y] * TILE);
-// 	if (!img.img_ptr)
-// 		return (img);
-// 	img.addr = mlx_get_data_addr(img.img_ptr, &img.bits_per_pixel,
-// 								&img.line_length, &img.endian);
-// 	//mlx_put_image_to_window(mlx.mlx, mlx.mlx_win, img.img_ptr, 0, 0);
-// 	return (img);
-// }
-
-int push_tile(t_mlx_data mlx, void *asset, int x_coord, int y_coord)
+int	render(t_data *data)
 {
-	int	x;
-	int	y;
-	
-	x = x_coord * TILE;
-	y = y_coord * TILE;
-	if (mlx_put_image_to_window(mlx.mlx, mlx.mlx_win, asset, x, y) == 0)
-		return (0);
-	return (1);
-}
-
-int	render_border(t_mlx_data mlx, t_map map, void **assets)
-{
-	int	i;
-	
-	i = 1;
-	if (push_tile(mlx, assets[T_TL], 0, 0) == 0
-		|| push_tile(mlx, assets[T_TR], map.size[X] - 1, 0) == 0
-		|| push_tile(mlx, assets[T_BL], 0, map.size[Y] - 1) == 0
-		|| push_tile(mlx, assets[T_BR], map.size[X] - 1, map.size[Y] - 1) == 0)
-		return (0);
-	while (i < map.size[X] - 1)
- 	{
-		if (push_tile(mlx, assets[T_TOP], i, 0) == 0
-			|| push_tile(mlx, assets[T_BOTT], i, map.size[Y] - 1) == 0)
-			return (0);
-		i++;
-	}
-	i = 1;
-	while (i < map.size[Y] - 1)
-	{
-		if (push_tile(mlx, assets[T_LEFT], 0, i) == 0
-			|| push_tile(mlx, assets[T_RIGHT], map.size[X] - 1, i) == 0)
-			return (0);
-		i++;
-	}
-	return (1);
-}
-
-int	render_fix_content(t_mlx_data mlx, t_map map, void **assets)
-{
-	int	x;
-	int	y;
 	int	ok;
 
-	x = 1;
-	y = 1;
-	ok = 1;
-	while (y < map.size[Y] - 1)
-	{
-		while (x < map.size[X] - 1)
-		{
-			if (map.map_data[y][x] == WALL)
-				ok = push_tile(mlx, assets[T_WALL], x, y);
-			if (map.map_data[y][x] == EXIT)
-				ok = push_tile(mlx, assets[T_EXIT], x, y);
-			if (map.map_data[y][x] == SPACE)
-				ok = push_tile(mlx, assets[T_SPACE], x, y);
-			if (ok == 0)
-				return (ft_dprintf(2, LOAD_ASSETS), 0);
-			x++;
-		}
-		x = 1;
-		y++;
-	}
-	return (1);
-}
-
-int	render_moving_content(t_mlx_data mlx, t_map map, void **assets)
-{
-	int	x;
-	int	y;
-	int	ok;
-
-	x = 1;
-	y = 1;
-	ok = 1;
-	while (y < map.size[Y] - 1)
-	{
-		while (x < map.size[X] - 1)
-		{	
-			if (map.map_data[y][x] == COIN)
-				ok = push_tile(mlx, assets[T_COIN], x, y);
-			if (map.map_data[y][x] == PLAYER)
-				ok = push_tile(mlx, assets[T_PLAYER], x, y);
-			if (map.map_data[y][x] == ENEMIES)
-				ok = push_tile(mlx, assets[T_ENEMY], x, y);
-			if (ok == 0)
-				return (ft_dprintf(2, LOAD_ASSETS), 0);
-			x++;
-		}
-		x = 1;
-		y++;
-	}
-	return (1);
+	ok = render_border(data);
+	ok = render_fix_content(data->mlx, data->map, data->assets);
+	ok = render_moving_content(data->mlx, data->map, data->assets);
+	ok = render_player(data);
+	// if (data->count == 100 && ok == 0)
+	// 	ft_dprintf(2, LOAD_ASSETS);
+	return (ok);
 }
---
+++ srcs/map_render_utils.c
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_render_utils.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jblaye <jblaye@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/14 15:18:11 by jblaye            #+#    #+#             */
+/*   Updated: 2024/02/16 19:33:22 by jblaye           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/so_long.h"
+
+int push_tile(t_mlx_data mlx, void *asset, int x_coord, int y_coord)
+{
+	int	x;
+	int	y;
+	
+	x = x_coord * TILE;
+	y = y_coord * TILE;
+	if (mlx_put_image_to_window(mlx.mlx, mlx.mlx_win, asset, x, y) == - 1)
+		return (0);
+	return (1);
+}
+
+int	render_border(t_data *data)
+{
+	int	i;
+	
+	i = 1;
+	if (push_tile(data->mlx, data->assets[T_TL], 0, 0) == 0
+		|| push_tile(data->mlx, data->assets[T_TR], data->map.size[X] - 1, 0) == 0
+		|| push_tile(data->mlx, data->assets[T_BL], 0, data->map.size[Y] - 1) == 0
+		|| push_tile(data->mlx, data->assets[T_BR], data->map.size[X] - 1, 
+					data->map.size[Y] - 1) == 0)
+		return (0);
+	while (i < data->map.size[X] - 1)
+ 	{
+		if (push_tile(data->mlx, data->assets[T_TOP], i, 0) == 0
+			|| push_tile(data->mlx, data->assets[T_BOTT], i, data->map.size[Y] - 1) == 0)
+			return (0);
+		i++;
+	}
+	i = 1;
+	while (i < data->map.size[Y] - 1)
+	{
+		if (push_tile(data->mlx, data->assets[T_LEFT], 0, i) == 0
+			|| push_tile(data->mlx, data->assets[T_RIGHT], data->map.size[X] - 1, i) == 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+int	render_fix_content(t_mlx_data mlx, t_map map, void **assets)
+{
+	int	x;
+	int	y;
+	int	ok;
+
+	x = 1;
+	y = 1;
+	ok = 1;
+	while (y < map.size[Y] - 1)
+	{
+		while (x < map.size[X] - 1)
+		{
+			if (map.map_data[y][x] == WALL)
+				ok = push_tile(mlx, assets[T_WALL], x, y);
+			if (map.map_data[y][x] == SPACE)
+				ok = push_tile(mlx, assets[T_SPACE], x, y);
+			if (map.map_data[y][x] == EXIT)
+				ok = push_tile(mlx, assets[T_EXIT], x, y);
+			if (ok == 0)
+				return (0);
+			x++;
+		}
+		x = 1;
+		y++;
+	}
+	return (1);
+}
+
+int	render_moving_content(t_mlx_data mlx, t_map map, void **assets)
+{
+	int	x;
+	int	y;
+	int	ok;
+
+	x = 1;
+	y = 1;
+	ok = 1;
+	while (y < map.size[Y] - 1)
+	{
+		while (x < map.size[X] - 1)
+		{	
+			if (map.map_data[y][x] == COIN)
+				ok = push_tile(mlx, assets[T_COIN], x, y);
+			if (map.map_data[y][x] == COL_COIN)
+				ok = push_tile(mlx, assets[T_COLLEC_COIN], x, y);
+			if (map.map_data[y][x] == OPEN_EXIT)
+				ok = push_tile(mlx, assets[T_OPEN_EXIT], x, y);
+			if (ok == 0)
+				return (0);
+			x++;
+		}
+		x = 1;
+		y++;
+	}
+	return (1);
+}
