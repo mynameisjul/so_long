817b45b0ea5e7b3b0429b827b7660c88d8b66d98
Julie Blaye <jblaye@paul-f3Ar2s2.clusters.42paris.fr>
2024-02-20 11:35:57 +0100

    modified main because data.level is -1 when we add a map argument

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: jblaye <jblaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 10:32:54 by julieblaye        #+#    #+#             */
-/*   Updated: 2024/02/20 10:12:55 by jblaye           ###   ########.fr       */
+/*   Updated: 2024/02/20 11:28:26 by jblaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int	main(int ac, char **av)
 			return (0);
 		data.level = -2;
 		terminate_level_window(&data);
-		if (data.win == BYE || data.level == -1)
+		if (data.win == BYE || data.level == -2)
 			return (0);
 		init_tmap(&(data.map));
 		if (map_parsing(av[1], &(data.map)) == 0 || find_path(&(data.map)) == 0)
