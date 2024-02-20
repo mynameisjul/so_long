71e170ef41df7ecca5b561f26908196894131d17
Julie Blaye <jblaye@paul-f3Ar2s2.clusters.42paris.fr>
2024-02-20 11:59:14 +0100

    modified the if conditions to also take into account the left enemies (char x)

--- srcs/valid_path.c
+++ srcs/valid_path.c
@@ -6,7 +6,7 @@
 /*   By: jblaye <jblaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 14:27:08 by jblaye            #+#    #+#             */
-/*   Updated: 2024/02/19 10:26:42 by jblaye           ###   ########.fr       */
+/*   Updated: 2024/02/20 11:45:59 by jblaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,17 @@ int recurs_path_find(char **m, t_map *cpmap, t_map map, int x, int y)
 	if (m[y][x] == COIN)
 		cpmap->nb_coins++;
 	m[y][x] = WALL;
-	if (y - 1 > 0 && !(m[y - 1][x] == '1' || (m[y - 1][x] == 'X' && BONUS == 1)))
+	if (y - 1 > 0 && !(m[y - 1][x] == '1'
+		|| ((m[y - 1][x] == 'X' || m[y - 1][x] == 'x')  && BONUS == 1)))
 		recurs_path_find(m, cpmap, map, x, y - 1);
-	if (y + 1 < map.size[Y]
-		&& !(m[y + 1][x] == '1' || (m[y + 1][x] == 'X' && BONUS == 1)))
+	if (y + 1 < map.size[Y] && !(m[y + 1][x] == '1'
+		|| ((m[y + 1][x] == 'X'|| m[y + 1][x] == 'x') && BONUS == 1)))
 		recurs_path_find(m, cpmap, map, x, y + 1);
-	if (x + 1 < map.size[X]
-		&& !(m[y][x + 1] == '1' || (m[y][x + 1] == 'X' && BONUS == 1)))
+	if (x + 1 < map.size[X] && !(m[y][x + 1] == '1'
+		|| ((m[y][x + 1] == 'X'|| m[y][x + 1] == 'x') && BONUS == 1)))
 		recurs_path_find(m, cpmap, map, x + 1, y);
-	if (x - 1 > 0 && !(m[y][x - 1] == '1' || (m[y][x - 1] == 'X' && BONUS == 1)))
+	if (x - 1 > 0 && !(m[y][x - 1] == '1'
+		|| ((m[y][x - 1] == 'X'|| m[y][x - 1] == 'x') && BONUS == 1)))
 		recurs_path_find(m, cpmap, map, x - 1, y);
 	return (0);
 }
