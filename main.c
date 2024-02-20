6a6ad6a52ed11afeb9051472f962891a903fab8b
Julie Blaye <jblaye@paul-f3Ar2s2.clusters.42paris.fr>
2024-02-20 12:00:44 +0100

    updated main from the test.c

--- main.c
+++ main.c
@@ -6,26 +6,30 @@
 /*   By: jblaye <jblaye@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/24 17:20:52 by jblaye            #+#    #+#             */
-/*   Updated: 2024/02/19 12:36:17 by jblaye           ###   ########.fr       */
+/*   Updated: 2024/02/20 12:00:15 by jblaye           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/so_long.h"
 
-int	main(void)
+int	main(int ac, char **av)
 {
 	t_data		data;
-	
+
 	init_tdata(&data);
-	while (data.level < 10)
+	if (ac == 2)
 	{
-		data.mlx = create_window(LEVEL_SIZE, LEVEL_SIZE);
+		data.mlx = create_window(LEVEL_SIZE, 4);
 		data.assets = assets_table(data.mlx.mlx);
 		if (!data.assets)
 			return (0);
+		data.level = -2;
 		terminate_level_window(&data);
-		if (map_parsing(level_char(data.level), &(data.map)) == 0 || find_path(&(data.map)) == 0)
+		if (data.win == BYE || data.level == -2)
 			return (0);
+		init_tmap(&(data.map));
+		if (map_parsing(av[1], &(data.map)) == 0 || find_path(&(data.map)) == 0)
+				return (0);
 		data.mlx = create_window(data.map.size[X], data.map.size[Y]);
 		if (!data.mlx.mlx_win)
 			return (ft_dprintf(2, WINDOW), 0);
@@ -33,13 +37,8 @@ int	main(void)
 		if (!data.assets)
 			return (0);
 		terminate_window(&data);
-		data.level++;
 	}
-	data.mlx = create_window(LEVEL_SIZE, LEVEL_SIZE);
-	data.assets = assets_table(data.mlx.mlx);
-	if (!data.assets)
-		return (0);
-	terminate_level_window(&data);
-	return (0);
+	else
+		return (launch_game(&data));
 	return (0);
 }
